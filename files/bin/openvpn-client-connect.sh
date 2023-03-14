c37dc258b89bdc2fa5a0e26bbf6956f2eb333f7e
Snider <snider@lt.hn>
2023-03-14 18:29:25 +0000

    openvpn client connect check cert CN matches username

--- files/bin/openvpn-client-connect.sh
+++ files/bin/openvpn-client-connect.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-# Get the IP address of the connected client
-CLIENT_IPv4=$trusted_ip
+echo "client-connect: common_name=$common_name, username=$username"
 
-# Add an iptables rule to block the client from accessing the openvpn server
-# however permit routing of everything else.
-iptables -I INPUT -s $CLIENT_IPv4 -d 127.0.0.1 -j DROP
-#iptables -I INPUT -s $CLIENT_IPv4 -d 10.8.0.1 -j DROP
+if [ "$common_name" == "$username" ]; then
+  echo "Client certificate common_name matches username. Allow connection."
+  # Get the IP address of the connected client
+  CLIENT_IPv4=$trusted_ip
+
+  # Add an iptables rule to block the client from accessing the openvpn server
+  # however permit routing of everything else.
+  iptables -I INPUT -s $CLIENT_IPv4 -d 127.0.0.1 -j DROP
+  #iptables -I INPUT -s $CLIENT_IPv4 -d 10.8.0.1 -j DROP
+  exit 0
+else
+  echo "Client certificate common_name does not match username. Reject connection."
+  exit 1
+fi
